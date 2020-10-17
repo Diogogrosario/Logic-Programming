7b3c0a16348307396265b5bea1080e47f171a3a1
Henrique <gamingwithapotato@gmail.com>
2020-10-17 16:22:57 +0100

    Started working on the board display

---
+++ board.pl
@@ -0,0 +1,69 @@
+initial([
+    [nodef, nodef, nodef, space, empty, empty, empty, empty, empty, nodef, nodef, nodef],
+    [nodef, nodef, empty, empty, empty, empty, empty, empty, empty, empty, nodef, nodef],
+    [nodef, space, empty, empty, empty, empty, empty, empty, empty, empty, empty, nodef],
+    [nodef, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, nodef],
+    [space, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty],
+    [empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty],
+    [space, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty],
+    [empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty],
+    [space, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty],
+    [nodef, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, nodef],
+    [nodef, space, empty, empty, empty, empty, empty, empty, empty, empty, empty, nodef],
+    [nodef, nodef, empty, empty, empty, empty, empty, empty, empty, empty, nodef, nodef],
+    [nodef, nodef, nodef, space, empty, empty, empty, empty, empty, nodef, nodef, nodef]
+]).
+
+cell_val(empty, ' ').
+cell_val(nodef, '      ').
+cell_val(space, '   ').
+
+
+display_top([]).
+display_top(H) :-
+    [V | T] = H,
+    (
+        (
+            V\=nodef,
+            V\=space,
+            write(' ____ ')
+        );
+        (
+            cell_val(V,X),
+            write(X)
+        )
+    ),
+    display_top(T).
+
+display_mid([]).
+display_mid(H) :-
+    [V | T] = H,
+    (
+        (
+            V\=nodef,
+            V\=space,
+            cell_val(V,X),
+            put_code(9585),write('  '), write(X), write('  ') , put_code(9586)
+        );
+        (
+            cell_val(V,X),
+            write(X)
+        )
+    ),
+    display_mid(T).
+
+
+display_line(H) :-
+    display_top(H), nl,
+    display_mid(H), nl.
+    
+
+display_game([],Player).
+display_game(GameState,Player) :-
+    [H | T] = GameState,
+    display_line(H),
+    display_game(T,Player).
+
+play :-
+    initial(GameState),
+    display_game(GameState,1).
