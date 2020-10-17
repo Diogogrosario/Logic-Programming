84bd01c0d1276e41b1039036c3117a3a8e4202fc
Henrique <gamingwithapotato@gmail.com>
2020-10-17 16:34:53 +0100

    Board is done

--- board.pl
+++ board.pl
@@ -2,7 +2,7 @@ initial([
     [nodef, nodef, nodef, space, empty, empty, empty, empty, empty, nodef, nodef, nodef],
     [nodef, nodef, empty, empty, empty, empty, empty, empty, empty, empty, nodef, nodef],
     [nodef, space, empty, empty, empty, empty, empty, empty, empty, empty, empty, nodef],
-    [nodef, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, nodef],
+    [nodef, empty, empty, empty, empty, orange, empty, empty, empty, empty, empty, nodef],
     [space, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty],
     [empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty],
     [space, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty],
@@ -14,6 +14,7 @@ initial([
     [nodef, nodef, nodef, space, empty, empty, empty, empty, empty, nodef, nodef, nodef]
 ]).
 
+cell_val(orange, 'O').
 cell_val(empty, ' ').
 cell_val(nodef, '      ').
 cell_val(space, '   ').
@@ -43,7 +44,7 @@ display_mid(H) :-
             V\=nodef,
             V\=space,
             cell_val(V,X),
-            put_code(9585),write('  '), write(X), write('  ') , put_code(9586)
+            put_code(9585),write(' '), write(X), write('  ') , put_code(9586)
         );
         (
             cell_val(V,X),
@@ -53,9 +54,27 @@ display_mid(H) :-
     display_mid(T).
 
 
+display_bottom([]).
+display_bottom(H) :-
+    [V | T] = H,
+    (
+        (
+            V\=nodef,
+            V\=space,
+            cell_val(V,X),
+            put_code(9586),write('____') , put_code(9585)
+        );
+        (
+            cell_val(V,X),
+            write(X)
+        )
+    ),
+    display_bottom(T).
+
 display_line(H) :-
     display_top(H), nl,
-    display_mid(H), nl.
+    display_mid(H), nl,
+    display_bottom(H), nl.
     
 
 display_game([],Player).
