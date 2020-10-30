0584f12c3a93aca12c54f6aee1f3fa7495f81f39
Henrique <gamingwithapotato@gmail.com>
2020-10-30 17:19:44 +0000

    Changed some code to make the board look better and added some images

--- board.pl
+++ board.pl
@@ -2,20 +2,20 @@
 :- dynamic initial/1.
 
 initial([
-    [nodef, nodef, nodef, nodef, nodef, purpleBoardNodef, purpleBoardNodef, purpleBoardNodef, purpleBoardNodef, purpleBoardNodef, nodef, nodef, nodef, nodef]   ,
-    [nodef, nodef, nodef, nodef, space, empty, empty, empty, empty, empty, nodef, nodef, nodef, nodef],
-    [nodef, nodef, orangeBoardNodef, empty, empty, empty, empty, empty, empty, empty, empty, greeneBoardSpace, nodef, nodef],
-    [nodef, nodef, orangeBoardSpace, empty, empty, empty, empty, empty, empty, empty, empty, empty, greeneBoardSpace, nodef],
-    [nodef, orangeBoardNodef, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, greeneBoardSpace, nodef],
-    [nodef, orangeBoardSpace, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, greeneBoardSpace],
-    [orangeBoardNodef, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, greeneBoardSpace],
-    [nodef, space, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, nodef],
-    [greenBoardNodef, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, orangeBoardSpace],
-    [nodef, greeneBoardSpace, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, orangeBoardSpace],
-    [nodef, greenBoardNodef, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, orangeBoardSpace, nodef],
-    [nodef, nodef, greeneBoardSpace, empty, empty, empty, empty, empty, empty, empty, empty, empty, orangeBoardSpace, nodef],
-    [nodef, nodef, greenBoardNodef, empty, empty, empty, empty, empty, empty, empty, empty, orangeBoardSpace, nodef, nodef],
-    [nodef, nodef, nodef, nodef, space, empty, empty, empty, empty, empty, nodef, nodef, nodef, nodef],
+    [nodef, nodef, nodef, nodef, nodef, purpleBoardNodef, purpleBoardNodef, purpleBoardNodef, purpleBoardNodef, purpleBoardNodef, nodef, nodef, nodef, nodef],
+    [nodef, nodef, nodef, nodef, space,                 empty, empty, empty, empty, empty,                         nodef, nodef, nodef, nodef],
+    [nodef, nodef, orangeBoardNodef,         empty, empty, empty, empty, empty, empty, empty, empty,               greenBoardSpace, nodef, nodef],
+    [nodef, nodef, orangeBoardSpace,      empty, empty, empty, empty, empty, empty, empty, empty, empty,           greenBoardSpace, nodef],
+    [nodef, orangeBoardNodef,          empty, empty, empty, empty, empty, empty, empty, empty, empty, empty,       greenBoardSpace, nodef],
+    [nodef, orangeBoardSpace,      empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty,    greenBoardSpace],
+    [orangeBoardNodef,         empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, greenBoardSpace],
+    [nodef, space,               empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty,      nodef],
+    [greenBoardNodef,          empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, orangeBoardSpace],
+    [nodef, greenBoardSpace,      empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty,     orangeBoardSpace],
+    [nodef, greenBoardNodef,           empty, empty, empty, empty, empty, empty, empty, empty, empty, empty,       orangeBoardSpace, nodef],
+    [nodef, nodef, greenBoardSpace,       empty, empty, empty, empty, empty, empty, empty, empty, empty,           orangeBoardSpace, nodef],
+    [nodef, nodef, greenBoardNodef,          empty, empty, empty, empty, empty, empty, empty, empty,               orangeBoardSpace, nodef, nodef],
+    [nodef, nodef, nodef, nodef, space,                 empty, empty, empty, empty, empty,                         nodef, nodef, nodef, nodef],
     [nodef, nodef, nodef, nodef, nodef, purpleBoardNodef, purpleBoardNodef, purpleBoardNodef, purpleBoardNodef, purpleBoardNodef, nodef, nodef, nodef, nodef]
 ]).
 
@@ -23,7 +23,7 @@ player(0).
 
 cell_val(orangeBoardSpace,'org').
 cell_val(orangeBoardNodef,'   org').
-cell_val(greeneBoardSpace,'grn').
+cell_val(greenBoardSpace,'grn').
 cell_val(greenBoardNodef,'   grn').
 cell_val(purpleBoardNodef,'prp  ').
 cell_val(orange, 'O').
@@ -88,11 +88,11 @@ display_line(H) :-
     display_bottom(H), nl.
     
 
-display_game([],Player).
-display_game(GameState,Player) :-
+display_board([],Player).
+display_board(GameState,Player) :-
     [H | T] = GameState,
     display_line(H),
-    display_game(T,Player).
+    display_board(T,Player).
 
 display_player(Player):-
     P is Player+1,
@@ -103,15 +103,24 @@ update_player(Player):-
     retract(player(_)),
     assert(player(P)).
 
-display_pieces :-
+display_remaining_pieces :-
     write('Current Orange Pieces Left: 42'), nl,
     write('Current Purple Pieces Left: 42'), nl,
     write('Current Green Pieces Left: 42'), nl.
 
+display_alliances:-
+    write('Player 1 alliances: To connect Orange: Orange-Green, to connect Green: Green-Purple, to connect Purple: Purple-Orange'), nl,
+    write('Player 2 alliances: To connect Orange: Orange-Purple, to connect Green: Green-Orange, to connect Purple: Purple-Green'), nl.
+
+display_game(GameState,Player):-
+    display_board(GameState,Player),
+    display_player(Player),
+    display_remaining_pieces,
+    display_alliances,
+    update_player(Player).
+
 play :-
     player(Player),
     initial(GameState),
-    display_game(GameState,Player),
-    display_player(Player),
-    display_pieces,
-    update_player(Player).
+    display_game(GameState,Player).
+    
