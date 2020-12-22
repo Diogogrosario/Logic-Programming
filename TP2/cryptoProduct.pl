b5c256256101875d69c4b092943ea5cf7a42ecbe
Henrique <gamingwithapotato@gmail.com>
2020-12-22 17:28:57 +0000

    Project 2 user input solution done

---
+++ TP2/cryptoProduct.pl
@@ -0,0 +1,34 @@
+:- use_module(library(lists)).
+:- use_module(library(clpfd)).
+
+setValDomain([],_,IntValue, IntValue).
+
+setValDomain([X|Rest], L, IntValue, Acc):-
+    X in L..9,
+    NextAcc #= Acc*10 + X,
+    setValDomain(Rest, 0, IntValue, NextAcc).
+
+printList([]).
+printList([X|R]):-
+    write(X),
+    printList(R).
+
+print_results(MultLeft,MultRight,Sol):-
+    printList(MultLeft),
+    write(' x '),
+    printList(MultRight),
+    write(' = '),
+    printList(Sol).
+
+crypto_user(MultLeft, MultRight, Sol):-
+    setValDomain(MultLeft,1, LeftVal, 0),
+    setValDomain(MultRight,1, RightVal, 0),
+    setValDomain(Sol,1, SolVal, 0),
+    
+    append(MultLeft,MultRight, L),
+    append(L, Sol, F),
+    remove_dups(F,NoDups),
+    all_distinct(NoDups),
+    SolVal #= LeftVal * RightVal,
+    labeling([],NoDups),
+    print_results(MultLeft,MultRight,Sol).
